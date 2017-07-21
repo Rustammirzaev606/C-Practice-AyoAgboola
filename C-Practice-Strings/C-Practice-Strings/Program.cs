3d2085c94ca3b8e7cc916cb018648962edc6da83
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-07-21 09:30:17 -0400

    Booo!

--- C-Practice-Strings/C-Practice-Strings/Program.cs
+++ C-Practice-Strings/C-Practice-Strings/Program.cs
@@ -37,12 +37,80 @@ namespace C_Practice_Strings
             Console.WriteLine($"The position of 'd' is {rhymeName.IndexOf("d")}");
             Console.ReadKey();
 
-            // 17
-            int a1 = rhymeName.IndexOf('a');
-            int a2 = rhymeName.IndexOf('a', (a1 + 1));
-            Console.WriteLine(a2);
+            // 17 WRONG WRONG WRONG!
+            //int a1 = rhymeName.IndexOf('a');
+            //int a2 = rhymeName.IndexOf('a', (a1 + 1));
+            //Console.WriteLine(a2);
+            //Console.ReadKey();
+            //int a3 = rhymeName.IndexOf("little");
+            //int a4 = a3 + "little".Length;
+            //int a5 = rhymeName.IndexOf("little", a4);
+            //Console.WriteLine($"Index of the 2nd position LITTLE is {a5}.");
+            //Console.ReadKey();
+
+            //19 wrong
+            //21 wrong
+            //Console.WriteLine("The length of Basketball! is {o}", "basketball!".Length);
+            //Console.ReadKey();
+
+            string rhyme = "Mary had a little lamb,\nlittle lamb,\nlittle lamb,\nMary had a little lamb that was as white as snow.";
+            string lilBig = rhyme.Replace("little", "big big");
+            Console.WriteLine(lilBig);
             Console.ReadKey();
 
+            string noLIL = rhyme.Replace("little", "");
+            Console.WriteLine(noLIL);
+            Console.ReadKey();
+
+            string phoneNumber = "111-222-3333";
+            int startPosition = 0;
+            int numbOfChars = 3;
+            string areaCode = phoneNumber.Substring(startPosition, numbOfChars);   //substring(startPosition, Length) cuts off characters fron the string using start position and the lengh.
+            Console.WriteLine(areaCode);
+            Console.ReadKey();
+
+            int newStart = 4;
+            int newLength = 8;
+            string newNumber = phoneNumber.Substring(newStart, newLength);
+            Console.WriteLine(newNumber);
+            Console.ReadKey();
+
+            int CenOffNumStart = 4;
+            int CenOffNumLength = 3;
+            string CentOffNum = phoneNumber.Substring(CenOffNumStart, CenOffNumLength);
+            Console.WriteLine(CentOffNum);
+            Console.ReadKey();
+
+            string howAreYou = "     how  are you?     ";
+            string noSpaceX = howAreYou.Replace(" ", "X");
+            //string noSpace = howAreYou.Remove(" ");
+            Console.WriteLine(noSpaceX);
+            Console.ReadKey();
+
+            string trimEnd1 = howAreYou.TrimEnd(); // .trim() removes 'white spaces' from the string. trim() removes it from the whole string, TrimStart() removes white spaces from the beginning of the string and stops as soon as it meets any other character in the string. trimEnd() starts trimming from the end to the start and stops as soon as it meets any other character.
+            Console.WriteLine(trimEnd1 + "bruh" );
+            Console.ReadKey();
+            //32 Formating string output:
+            string q1 = "hello";
+            string q2 = "jonathan";
+            //s1 = s1.PadRight(s1.Length + (s2.Length - s1.Length));
+            Console.WriteLine(q1.PadLeft(q1.Length + (q2.Length - q1.Length), '.') );
+            Console.WriteLine(q2);
+            Console.ReadKey();
+            //35
+            string x1 = "Mary";
+            string x2 = "had";
+            string x3 = "a";
+            string x4 = "little";
+            string x5 = "lamb";
+            Console.WriteLine($"{x1,10}");
+            Console.WriteLine($"{x2,10}");
+            Console.WriteLine($"{x3,10}");
+            Console.WriteLine($"{x4,10}");
+            Console.WriteLine($"{x5,10}");
+            Console.ReadKey();
+
+            
 
         }
     }
