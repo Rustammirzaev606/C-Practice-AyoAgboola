d6f001b56f44b19057f450a0852f21bfeee7db6d
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-07-21 15:09:58 -0400

    YAY!

---
+++ C_Practice_Arrays/C_Practice_Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Practice_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bruh = new int[50];
+            Console.WriteLine(bruh.Length);
+            
+            Console.WriteLine(bruh.Rank);
+            Console.ReadLine();
+        }
+    }
+}
