a7bf735ad51f109954394152da8850574171bade
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-07-25 08:53:22 -0400

    Zxzxz

--- C_Practice_Arrays/C_Practice_Arrays/Program.cs
+++ C_Practice_Arrays/C_Practice_Arrays/Program.cs
@@ -15,6 +15,19 @@ namespace C_Practice_Arrays
             
             Console.WriteLine(bruh.Rank);
             Console.ReadLine();
+            double[] burh1 = new double[] { 1.4e9, 1.39e9, 42, 43.8, 8, 23.64, 18, 200 };
+            Console.WriteLine($"The array is : {burh1[0]} {burh1[1]} {burh1[2]} {burh1[3]} {burh1[4]} {burh1[5]} {burh1[6]} {burh1[7]}");
+            Console.ReadKey();
+            Array.Sort(burh1);
+            Console.WriteLine($"Now the array is {burh1[0]} {burh1[1]} {burh1[2]} {burh1[3]} {burh1[4]} {burh1[5]} {burh1[6]} {burh1[7]}");
+            Console.ReadKey();
+            int[] array99 = new int[] { 17, 42, 43, 8, 23 };
+            Console.WriteLine($"{array99[0]} {array99[1]} {array99[2]} {array99[3]} {array99[4]}");
+            Array.Resize<int>(ref array99, 7);
+            array99[5] = 57;
+            array99[6] = 84;
+            Console.WriteLine($"{array99[0]} {array99[1]} {array99[2]} {array99[3]} {array99[4]}  {array99[5]}  {array99[6]}");
+            Console.ReadKey();
         }
     }
 }
